c7262ed4417ea633894ff09f264fcb489e7597a5
Krzysztof Wal <krzysztof.j.wal@gmail.com>
2021-05-14 17:44:18 +0200

    Uporządkowanie nazw

--- my_rmi/src/my_rmi/MyCLientCallback.java
+++ my_rmi/src/my_rmi/MyCLientCallback.java
@@ -4,11 +4,11 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 
-public class MyCLientCallback  extends UnicastRemoteObject implements ClientCallbackInterface{
+public class MyClientCallback  extends UnicastRemoteObject implements ClientCallbackInterface{
 	
 	private static final long serialVersionUID = 7897576664568313628L;
 
-	public MyCLientCallback() throws RemoteException{
+	public MyClientCallback() throws RemoteException{
 		super();
 	}
 
